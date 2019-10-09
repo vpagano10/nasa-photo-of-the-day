10a3cd98be1b45bcff933f3102e2fecb183778d7
vpagano10 <vpagano10@gmail.com>
2019-10-09 17:41:05 -0400

    minor stlying change

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
   return (
     <div className="App">
       <p className='page-title'>
-        Astronomy Picture of the Day <span role="img" aria-label="rocketship">🚀</span>!
+        Astronomy Picture of the Day <span role="img" aria-label="rocketship"><a href="https://www.nasa.gov/multimedia/imagegallery/iotd.html">🚀</a></span>!
       </p>
       <div className='photo-container'>
         <PhotoList />
